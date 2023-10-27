5aea2ff1f9cd8e043a60420b91da6d1d2a2bd966
Alertyz <alec_meier@hotmail.com>
2023-10-27 17:32:47 -0300

    graph

--- graph.py
+++ graph.py
@@ -1,68 +1,96 @@
+# Imports
 import streamlit as st
 import pandas as pd
-import numpy as np
 import plotly.express as px
 
-# Configuração da página
+# Set Streamlit page configuration
 st.set_page_config(layout="wide")
 
-# Carregar dados
-df_inc = pd.read_csv("fires.csv", sep=';')
-df_ind = pd.read_csv("dataset_indigenas.csv", sep=';', encoding='latin-1', decimal=',')
+# Class Definition
+class DeforestationAnalysis:
+    def __init__(self):
+        self.df_inc, self.df_ind = self.load_data()
+        self.sidebar_filters()
 
-# Extrair o ano da coluna 'date' e criar uma nova coluna 'year'
-df_inc['year'] = df_inc['date'].apply(lambda x: int(x.split('/')[0]))
+    def load_data(self):
+        """Load the datasets."""
+        df_inc = pd.read_csv("fires.csv", sep=';')
+        df_ind = pd.read_csv("dataset_indigenas.csv", sep=';', encoding='latin-1', decimal=',')
+        df_inc['year'] = df_inc['date'].str.split('/').str[0].astype(int)
+        df_inc.rename(columns={'class': 'Classe'}, inplace=True)
+        return df_inc, df_ind
 
-# Renomear colunas
-df_inc.rename(columns={'class': 'Classe'}, inplace=True)
+    def sidebar_filters(self):
+        """Create sidebar filters."""
+        st.sidebar.header("Filtros")
+        years = ["Todos"] + sorted(self.df_ind["ANO"].unique().tolist(), reverse=True)
+        self.ano_selected = st.sidebar.selectbox("Selecione o ano", years)
+        classes = ["Todos"] + self.df_inc["Classe"].unique().tolist()
+        self.classe_selected = st.sidebar.multiselect("Selecione as classes de desmatamento", classes, default=["Todos"])
+        states = ["Todos"] + self.df_inc["uf"].unique().tolist()
+        self.estado_selected = st.sidebar.multiselect("Selecione os estados", states, default=["Todos"])
 
+    def run(self):
+        """Main run function to execute the dashboard."""
+        st.title("Análise de Desmatamento")
 
-# Sidebar
-st.sidebar.header("Filtros")
-ano_selected = st.sidebar.selectbox("Selecione o ano", df_ind["ANO"].unique())
-classe_selected = st.sidebar.multiselect("Selecione as classes de desmatamento", df_inc["Classe"].unique())
-estado_selected = st.sidebar.multiselect("Selecione os estados", df_inc["uf"].unique())
+        # Filtering data based on user inputs
+        df_inc_filtered = self.filter_inc_data()
+        df_ind_selected = self.filter_ind_data()
 
-# Filtros baseados na seleção da sidebar
-df_inc_filtered = df_inc[df_inc["Classe"].isin(classe_selected) & df_inc["uf"].isin(estado_selected)]
-df_ind_selected = df_ind[df_ind["ANO"] == ano_selected]
+        # Display plots
+        col1, col2 = st.columns(2)
+        with col1:
+            self.plot_classe_distribution(df_inc_filtered)
+        with col2:
+            self.plot_state_distribution(df_inc_filtered)
+        self.plot_area_by_tribe(df_ind_selected)
+        self.plot_annual_comparison(df_inc_filtered, df_ind_selected)
 
-# Criação de gráficos
-st.title("Análise de Desmatamento")
+    def filter_inc_data(self):
+        """Filter the 'inc' dataset based on user input."""
+        if "Todos" in self.classe_selected:
+            df_filtered = self.df_inc.copy()
+        else:
+            df_filtered = self.df_inc[self.df_inc["Classe"].isin(self.classe_selected)]
+        if "Todos" not in self.estado_selected:
+            df_filtered = df_filtered[df_filtered["uf"].isin(self.estado_selected)]
+        return df_filtered
 
-# Gráfico de desmatamento por classe
-if classe_selected:
-    st.subheader("Tipos de Desmatamentos")
-    fig = px.pie(df_inc_filtered, names='Classe', title='Distribuição dos tipos de desmatamento')
-    st.plotly_chart(fig)
+    def filter_ind_data(self):
+        """Filter the 'ind' dataset based on user input."""
+        if self.ano_selected != "Todos":
+            return self.df_ind[self.df_ind["ANO"] == int(self.ano_selected)]
+        else:
+            return self.df_ind.copy()
 
-# Gráfico de desmatamento por estado
-if estado_selected:
-    st.subheader("Distribuição de Desmatamentos por Estados")
-    fig = px.pie(df_inc_filtered, names='uf', title='Distribuição dos desmatamentos por estado')
-    st.plotly_chart(fig)
+    def plot_classe_distribution(self, df):
+        """Plot distribution of deforestation types."""
+        if not df.empty:
+            fig = px.pie(df, names='Classe', title='Distribuição dos tipos de desmatamento')
+            st.plotly_chart(fig)
 
-# Gráfico de área desmatada por ano
-st.subheader(f"Área desmatada em {ano_selected}")
-fig = px.bar(df_ind_selected, x='TRIBO', y='AREA', title=f'Área desmatada por tribo em {ano_selected}')
-st.plotly_chart(fig)
+    def plot_state_distribution(self, df):
+        """Plot distribution of deforestation by states."""
+        if not df.empty:
+            fig = px.pie(df, names='uf', title='Distribuição dos desmatamentos por estado')
+            st.plotly_chart(fig)
 
-# Comparação entre quantidade de desmatamentos e área perdida
-st.subheader("Comparação entre quantidade de desmatamentos e área perdida")
-# Lista de anos
-anos = df_ind["ANO"].unique()
+    def plot_area_by_tribe(self, df):
+        """Plot deforested area by tribe."""
+        fig = px.bar(df, x='TRIBO', y='AREA', title=f'Área desmatada por tribo em {self.ano_selected}')
+        st.plotly_chart(fig)
 
-# Cálculo de valores agregados para cada ano
-quantidade_desmatamentos = [df_inc[df_inc["year"] == ano].shape[0] for ano in anos]
-area_perdida = [df_ind[df_ind["ANO"] == ano]["AREA"].sum() for ano in anos]
+    def plot_annual_comparison(self, df_inc, df_ind):
+        """Plot annual comparison between deforestation quantity and lost area."""
+        df_inc_agg = df_inc.groupby("year").size().reset_index(name="Quantidade de Desmatamentos")
+        df_ind_agg = df_ind.groupby("ANO")["AREA"].sum().reset_index(name="Area Perdida Total")
+        df_comparacao = pd.merge(df_inc_agg, df_ind_agg, left_on="year", right_on="ANO", how="outer").fillna(0)
+        df_comparacao.drop(columns="ANO", inplace=True) 
+        fig = px.bar(df_comparacao, x="year", y=["Quantidade de Desmatamentos", "Area Perdida Total"], barmode="group", title="Comparação Anual")
+        st.plotly_chart(fig)
 
-df_comparacao = pd.DataFrame({
-    "Ano": anos,
-    "Quantidade de Desmatamentos": quantidade_desmatamentos,
-    "Area Perdida Total": area_perdida
-})
-
-fig = px.bar(df_comparacao, x="Ano", y=["Quantidade de Desmatamentos", "Area Perdida Total"], title="Comparação Anual")
-st.plotly_chart(fig)
-
-# Você pode adicionar mais gráficos e informações conforme necessário
+# Main Execution
+if __name__ == '__main__':
+    analysis = DeforestationAnalysis()
+    analysis.run()
